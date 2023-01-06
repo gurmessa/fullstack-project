1d3191693a42416a988f0ceeb8c5b28a900a6786
Gurmessa Lemma <gurmessalemma@gmail.com>
2023-01-06 18:52:26 +0300

    update store/rootReducer.ts

--- frontend/store/rootReducer.ts
+++ frontend/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import feedbackReducer from './feedback/feedbackSlice'
+import feedbackDetailReducer from './feedbackDetail/feedbackDetailSlice'
 
 const rootReducer = combineReducers({
   user: userReducer,
   feedback: feedbackReducer,
+  feedbackDetail: feedbackDetailReducer,
 })
 export type RootState = ReturnType<typeof rootReducer>
 export default rootReducer
