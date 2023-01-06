5e31f5474e42d5c99e2fe9c26b3d7c903d167f89
Gurmessa Lemma <gurmessalemma@gmail.com>
2023-01-06 08:48:07 +0300

    create PickupFeedbackSerializer

--- backend/project/serializers.py
+++ backend/project/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from project.models import Essay, FeedbackRequest, Feedback
@@ -37,5 +38,26 @@ class ReturnFeedbackSerializer(serializers.ModelSerializer):
 	def update(self, instance, validated_data):
 		instance.comment = validated_data.get('comment', instance.comment)
 		instance.status = Feedback.RETURN_FEEDBACK
+		instance.ended_at = timezone.now()
 		instance.save()
 		return instance
+
+
+class PickupFeedbackSerializer(serializers.ModelSerializer):
+	""" Serialize a Feedback. """
+	
+	class Meta:
+		model = Feedback
+		fields = ('feedback_request', )
+
+	def create(self, validated_data):
+		feedback_request = validated_data['feedback_request']
+		essay = feedback_request.essay 
+		feedback = Feedback.objects.create(
+			essay=essay,
+			feedback_request=feedback_request,
+			started_at=timezone.now(),
+			edited_by=self.context['request'].user
+		)
+		return feedback
+
